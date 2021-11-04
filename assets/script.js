5adc697f85688fa5bb30d4297d14922274917f69
colemunson <colermunson@gmail.com>
2021-11-03 20:33:38 -0500

    testing

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,5 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Cole Munson" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
@@ -10,14 +7,39 @@ let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
 
 // Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+document.getElementById('credit').textContent = `Created by ${"Cole Munson"}`
+
+
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+document.getElementById('add-gb').addEventListener('click', function(x) {
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    document.getElementById('qty-gb').textContent = parseInt(document.getElementById('qty-gb').textContent, 10) + 1
+    
+})
+document.getElementById('minus-gb').addEventListener('click', function(x) {
+
+    document.getElementById('qty-gb').textContent = parseInt(document.getElementById('qty-gb').textContent, 10) - 1
+    
+})
+document.getElementById('add-cc').addEventListener('click', function(x) {
+
+    document.getElementById('qty-cc').textContent = parseInt(document.getElementById('qty-cc').textContent, 10) + 1
+    
+})
+document.getElementById('minus-cc').addEventListener('click', function(x) {
+
+    document.getElementById('qty-cc').textContent = parseInt(document.getElementById('qty-cc').textContent, 10) - 1
+    
+})
+document.getElementById('add-sugar').addEventListener('click', function(x) {
+
+    document.getElementById('qty-total').textContent = parseInt(document.getElementById('qty-total').textContent, 10) + 1
+    
+})
+document.getElementById('minus-sugar').addEventListener('click', function(x) {
+
+    document.getElementById('qty-total').textContent = parseInt(document.getElementById('qty-total').textContent, 10) - 1
+    
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
